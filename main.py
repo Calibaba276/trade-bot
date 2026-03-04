a65dade01384f0dc918164f7936362fd6a5c0788
Calibaba-Herit <calebabs207@gmail.com>
2026-03-04 13:52:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,9 @@ start = datetime.strptime(BACKTESTING_START, "%Y-%m-%d")
 end = datetime.strptime(BACKTESTING_END, "%Y-%m-%d")
 
 if __name__ == "__main__":
-    if os.getenv("ISBACKTESTING") == "true":
-        strategy = TradingStrategy(
-            name="Liquidity Sweep",
-            parameters={"symbol": "EURUSD=X"}
-        )
-
-        strategy.backtest(
-            YahooDataBacktesting,
-            start,
-            end
-        )
+    TradingStrategy.run_backtest(
+        YahooDataBacktesting,
+        start,
+        end,
+        parameters={"symbol": "EURUSD=X"}
+    )
