1e50bb8004a58f3dcef1c3457f037ab356e9df5c
Calibaba-Herit <calebabs207@gmail.com>
2026-03-04 16:24:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
         end,
         parameters={"symbol": "C:EURUSD"},
         quiet_logs=False,
-        api_key=POLYGON_API_KEY
+        polygon_api_key=POLYGON_API_KEY
     )
