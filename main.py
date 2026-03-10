13cbcc590e30acaf0013cc1ef2a005e2ea1483b9
Calibaba-Herit <calebabs207@gmail.com>
2026-03-10 16:19:43 +0100

    Support Alpaca creds and boolean backtest flag
    Parse ISBACKTESTING as a boolean with a default of false and defer parsing BACKTESTING_START/END until the backtest branch to avoid errors when those env vars are absent. Add ALPACA_API_KEY and ALPACA_API_SECRET env variables and pass them (with PAPER=true) into the Alpaca broker initialization. Also set a default LiquiditySweep parameter (symbol: EURUSD) when running live.

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ from datetime import datetime
 
 load_dotenv()
 
-ISBACKTESTING = os.getenv("ISBACKTESTING")
+ISBACKTESTING = os.getenv("ISBACKTESTING", "false").lower() == "true"
 BACKTESTING_START = os.getenv("BACKTESTING_START")
 BACKTESTING_END = os.getenv("BACKTESTING_END")
 POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
-
-start = datetime.strptime(BACKTESTING_START, "%Y-%m-%d")
-end = datetime.strptime(BACKTESTING_END, "%Y-%m-%d")
+ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
+ALPACA_API_SECRET = os.getenv("ALPACA_API_SECRET")
 
 if __name__ == "__main__":
     if ISBACKTESTING:
+
+        start = datetime.strptime(BACKTESTING_START, "%Y-%m-%d")
+        end = datetime.strptime(BACKTESTING_END, "%Y-%m-%d")
+
         LiquiditySweep.run_backtest(
             PolygonDataBacktesting,
             start,
@@ -30,8 +33,13 @@ if __name__ == "__main__":
             polygon_api_key=POLYGON_API_KEY
         )
     else:
-        broker = Alpaca()
-        strategy = LiquiditySweep(broker=broker)
+        broker = Alpaca({
+            "API_KEY": ALPACA_API_KEY,
+            "API_SECRET": ALPACA_API_SECRET,
+            "PAPER": True
+        })
+
+        strategy = LiquiditySweep(broker=broker, parameters={"symbol": "EURUSD"})
         trader = Trader()
         trader.add_strategy(strategy)
         trader.run_all()
