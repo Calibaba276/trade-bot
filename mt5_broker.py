2abb75677f526fb19213ec12ba6e66fe09d0a446
Caleb Babatunde <62670850+Calibaba276@users.noreply.github.com>
2026-03-20 17:59:05 +0100

    Update mt5_broker.py

--- mt5_broker.py
+++ mt5_broker.py
@@ -71,8 +71,7 @@ class MetaTrader5(Broker):
 
                 lumibot_positions.append(Position(
                     asset=asset,
-                    quantity=pos.volume,
-                    side=side
+                    quantity=pos.quantity
                 ))
 
                 print(f"TRACKED POSITION: {pos.symbol} | Quantity: {pos.volume} | Entry: {pos.price_open}")
