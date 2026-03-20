3b5c9c8adc40ada6ff0e2b05f0368cd1aa3e3672
Caleb Babatunde <62670850+Calibaba276@users.noreply.github.com>
2026-03-20 14:18:49 +0100

    Update mt5_broker.py

--- mt5_broker.py
+++ mt5_broker.py
@@ -88,10 +88,11 @@ class MetaTrader5(Broker):
         
         return tick.last if tick.last != 0 else (tick.bid + tick.ask) / 2
     
-    def get_historical_prices(self, asset, length, timestep="minute", *args, **kwargs):
+    def get_historical_prices(self, asset, length, timestep, *args, **kwargs):
         """Required for technical indicators and strategy logic"""
 
         tf_map = {"minute": mt5.TIMEFRAME_M1, "hour": mt5.TIMEFRAME_H1, "day": mt5.TIMEFRAME_D1}
+
         timeframe = tf_map.get(timestep)
         if timeframe is None:
             raise ValueError(f"Unsupported timestep: {timestep}. Use one of: {', '.join(tf_map.keys())}")
@@ -99,6 +100,7 @@ class MetaTrader5(Broker):
         rates = mt5.copy_rates_from_pos(asset.symbol, timeframe, 0, length)
 
         if rates is None:
+            raise ValueError("No response receieved... rates are none")
             return pd.DataFrame()
         
         df = pd.DataFrame(rates)
