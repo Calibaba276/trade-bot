19a2100bd425a596147113e51dca5c82dc09d103
Caleb Babatunde <62670850+Calibaba276@users.noreply.github.com>
2026-03-20 19:27:17 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -30,12 +30,12 @@ mt5 = MetaTrader5(
 )
 
 if __name__ == "__main__":
-    symbol = "AAPLm"
+    symbol = "UBERm"
 
     # print(mt5.select_symbol(Asset(symbol, asset_type="stock")))
     # print(mt5.get_historical_prices(Asset(symbol, asset_type="stock"), 100, "minute"))
 
     print(mt5api.symbol_info(symbol))
 
-    # last_tick = mt5api.symbol_info_tick(symbol)
-    # print(f"Last Price: {last_tick.ask}")
+    last_tick = mt5api.symbol_info_tick(symbol)
+    print(f"Last Price: {last_tick.ask}")
